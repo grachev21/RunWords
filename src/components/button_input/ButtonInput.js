8112ac9feb51172d92040c54dc4c56da61c6c4dd
grachev21 <grachev613@gmail.com>
2024-05-17 07:05:01 +0300

    react-router-dom

---
+++ src/components/button_input/ButtonInput.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+
+export default function ButtonInput() {
+    return (
+        <div className="ButtonInput">
+
+        </div>
+    )
+}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,20 @@
 import day from '../../img/icons/day.png';
 import night from '../../img/icons/night.png';
+import logo from '../../img/icons/ravencoin.png';
+import menu from '../../img/icons/menu_header.png'
 import './styles.css'
 
 
 export default function Nav() {
     return (
         <header>
-            <div className='logo'>WorSa</div>
+            <div className='block_menu_header'>
+                <img className='menu_header' src={menu} />
+                <div className="logo">
+                    <img src={logo} />
+                    <div className='logotext'>WorSa</div>
+                </div>
+            </div>
             <div className="switch_block">
                 <img className='icons' src={day} />
                 <img className='icons' src={night} />
