363a7256e319aef8a41c3a939a3f527fadc77bdd
grachev21 <grachev613@gmail.com>
2024-05-24 18:37:36 +0400

    first commit

--- src/components/button_theme/ButtonTheme.js
+++ src/components/button_theme/ButtonTheme.js
@@ -4,6 +4,7 @@ import night from './icons/night.png';
 import './styles.css';
 
 
+
 export default function ButtonTheme() {
 
     const [Dark, setDark] = useState('light');
@@ -12,12 +13,11 @@ export default function ButtonTheme() {
     useEffect(() => {
 
         if (Dark === 'dark') {
-            document.querySelector('html').classList.remove('light');
             document.querySelector('html').classList.add('dark');
             btnRef.current.classList.add('dark-button-active');
+
         } else {
             document.querySelector('html').classList.remove('dark');
-            document.querySelector('html').classList.add('light');
             btnRef.current.classList.remove('dark-button-active');
         }
     }, [Dark]);
@@ -34,7 +34,7 @@ export default function ButtonTheme() {
                 <img className='icons' src={day} />
                 <img className='icons' src={night} />
             </button>
-            
+
         </div>
     )
 }
